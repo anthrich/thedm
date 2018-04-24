b8671b3da162e6b5de0b691e830e2d8b62999aa2
Unknown <anth@technicallycompatible.com>
2018-04-24 22:52:53 +0100

    Updated .gitignore

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+
+module.exports = {
+    entry: './init.ts',
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: "style-loader" // creates style nodes from JS strings
+                    }, {
+                        loader: "css-loader" // translates CSS into CommonJS
+                    }, {
+                        loader: "sass-loader" // compiles Sass to CSS
+                    }
+                ]
+            },
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            },
+            {
+                test:/\.(html|tsv)/,
+                use: [{
+                    loader: 'file-loader',
+                    options: { name: '[name].[ext]' }
+                }]
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {}
+                    }
+                ]
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, './dist')
+    }
+};
